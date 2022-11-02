7099e4e114d466846f1c68b1198b7f05dd22c5ea
U480401 <adrissi@bptech.ma>
2022-11-02 14:03:10 +0100

    Initial commit

---
+++ src/test/java/com/example/zawyaconfigservice/ZawyaConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zawyaconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZawyaConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
